5cdec53e00c7ef2ce46724b0ce2e3a413bcd722e
Juliane Marubayashi <juliane_maru@yahoo.com.br>
2018-06-28 02:31:30 -0300

    Update

--- Teste.py
+++ Teste.py
@@ -1,3 +1,10 @@
-lista = [1,2,3,4]
-
-print(lista[3:], lista[:3])
+j, r = map(int, input().split())
+jogadores = [0 for k in range(j)]
+lista = list(map(int, input().split()))
+contador = 0
+for i in lista:
+    jogadores[contador]+=i
+    contador+=1
+    if contador%j==0:
+        contador = 0
+print(jogadores.index(max(jogadores))+1)
