d007463f99b474f910907c1333c3485274b95113
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-01-23 14:12:11 +0000

    Create CPP_URI_1009.cpp

---
+++ CPP/CPP_URI_1009.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string nome;
+    double fixo, comissao;
+    cin >> nome;
+    cin >> fixo >> comissao;
+    cout << fixed << setprecision(2);
+    cout << "TOTAL = R$ " << fixo+comissao*0.15 << endl;
+
+    return 0;
+}
