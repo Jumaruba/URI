4c7853bafee90ff835bdb96d7096ec29bb8959bb
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-01-23 13:56:16 +0000

    Create CPP_URI_1006.cpp

---
+++ CPP/CPP_URI_1006.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed << setprecision(1);
+    cout << "MEDIA = " <<(a*2+b*3+c*5)/10  << endl;
+    return 0;
+}
