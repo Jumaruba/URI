3df63ce8b4cc2ee2f7c341ff12315580f6a355eb
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-01-23 12:24:36 +0000

    Create CPP_URI_1002.cpp

---
+++ CPP_URI_1002.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <iomanip>    //setprecision
+using namespace std;
+
+int main()
+{
+    double pi = 3.14159, area, raio;
+    cin >> raio;
+    cout << fixed << setprecision(4);   //to limit decimals
+    cout << "A=" << pi*raio*raio << endl;
+
+    return 0;
+}
