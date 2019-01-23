8f5e8ef520f09ace00b576c3255a5c12354eef21
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-01-23 16:30:11 +0000

    Create CPP_URI_1020.cpp

---
+++ CPP/CPP_URI_1020.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dias;
+    cin >> dias;
+    cout << dias/365 << " ano(s)" << endl;
+    dias = dias%365;
+    cout << dias/30 << " mes(es)" << endl;
+    dias = dias%30;
+    cout << dias << " dia(s)" << endl;
+    return 0;
+}
+
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1020
