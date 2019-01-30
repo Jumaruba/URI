a6d759550efc0cbc287657bf7efed6c663b77d02
Juliane Marubayashi <33182421+Jumaruba@users.noreply.github.com>
2019-01-30 15:49:34 +0000

    Create CPP_URI_1793.cpp

---
+++ CPP/CPP_URI_1793.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int pessoas, tempo, momento, k;
+    cin >> pessoas;
+    while (pessoas!=0){
+        for (int i = 0; i< pessoas; i++){
+            cin >> k;
+            if (i == 0)
+                momento = k;
+                
+            else {
+                if (momento + 10 < k){
+                    tempo += 10;
+                    momento = k;
+                }
+                else{
+                    tempo += k-momento;
+                    momento = k;
+                }
+
+            }
+        }
+        tempo += 10;
+        cout << tempo << endl;
+        tempo = 0;
+        cin >> pessoas;
+    }
+    return 0;
+}
